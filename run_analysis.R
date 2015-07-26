1bdfc5fbd0fcf5c3e5bfd7dbb68a260fbd847aca
Naman Mittal <namanmittal003@gmail.com>
2015-07-26 01:59:38 -0400

    Edited Code to be ran from the working directory

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
 library(plyr)
 
 #############Read X_test and X_Train data############
-x_data_test<- read.table("./Documents/UCI HAR Dataset/test/X_test.txt",header = F)
-x_data_train <- read.table("./Documents/UCI HAR Dataset/train/X_train.txt",header = F)
+x_data_test<- read.table("./test/X_test.txt",header = F)
+x_data_train <- read.table("./train/X_train.txt",header = F)
 ###########Read the feature data to apply column names###############
-feature <- read.table("./Documents/UCI HAR Dataset/features.txt", header = F)
+feature <- read.table("./features.txt", header = F)
 #####################Set Column Names (Headers) for the test and train data##############
 colnames(x_data_test) <- feature[,2]
 colnames(x_data_train) <- feature[,2]
@@ -12,8 +12,9 @@ colnames(x_data_train) <- feature[,2]
 data_x <- rbind(x_data_test,x_data_train)
 
 ###################Read Y_Test and Y_Train Data##################
-y_data_test<- read.table("./Documents/UCI HAR Dataset/test/y_test.txt",header = F)
-y_data_train <- read.table("./Documents/UCI HAR Dataset/train/y_train.txt",header = F)
+y_data_test<- read.table("./test/y_test.txt",header = F)
+y_data_train <- read.table("./train/y_train.txt",header = F)
+
 ################Aplly headers to y_test and y_train data############
 colnames(y_data_test) <- "activityLabel"
 colnames(y_data_train) <- "activityLabel"
@@ -21,8 +22,8 @@ colnames(y_data_train) <- "activityLabel"
 data_y <- rbind(y_data_test,y_data_train)
 
 #####################Read Subject test and train data##############
-subject_data_test<- read.table("/home/naman/Desktop/Documents/UCI HAR Dataset/test/subject_test.txt",header = F)
-subject_data_train <- read.table("/home/naman/Desktop/Documents/UCI HAR Dataset/train/subject_train.txt",header = F)
+subject_data_test<- read.table("./test/subject_test.txt",header = F)
+subject_data_train <- read.table("./train/subject_train.txt",header = F)
 ##################Add column name to test and train data and thne combine them##########
 colnames(subject_data_test) <- "subjectId"
 colnames(subject_data_train) <- "subjectId"
@@ -37,7 +38,7 @@ tidyDataSet = combineddata[,grep(patrn , names(combineddata), value=TRUE)]
 
 
 ############Read the activity labels and substitute them in the tidy data set##########
-activityLabel <- read.table("/home/naman/Desktop/Documents/UCI HAR Dataset/activity_labels.txt",header = F)
+activityLabel <- read.table("./activity_labels.txt",header = F)
 tidyDataSet$activityLabel <- factor(tidyDataSet$activityLabel,labels = tolower(levels(activityLabel$V2)))
 
 
